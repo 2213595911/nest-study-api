b62168c0c4695e1a3dedd51a23a0823f0d7dcdc2
luca <2213595911@qq.com>
2022-02-14 18:35:21 +0800

    配置文件全局统一管理

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import {AppService} from './app.service';
 import {PostsModule} from './modules/posts/posts.module';
 import {MongooseModule} from '@nestjs/mongoose';
 import {UsersModule} from './modules/users/users.module';
-import {ConfigModule} from "@nestjs/config";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 import {LoginModule} from './modules/login/login.module';
 import {MulterModule} from "@nestjs/platform-express";
 import {ServeStaticModule} from "@nestjs/serve-static";
@@ -12,18 +12,27 @@ import {join} from 'path'
 import {ExceptionModule} from './modules/exception/exception.module';
 import {GuardModule} from "./modules/guard/guard.module";
 import {EmailModule} from "./modules/email/email.module";
+import configModule from './config/index'
+import {MailerModule} from "@nestjs-modules/mailer";
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/nest-blog-api'),
-    // 全局env
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
+
     // 上传文件
     MulterModule.register({
       dest: 'uploads/',
     }),
+    // 配置文件
+    ConfigModule.forRoot({
+      load: [configModule],
+      isGlobal: true
+    }),
+    MailerModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get('email'),
+      inject: [ConfigService]
+    }),
+    // 静态资源
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../uploads'),
       serveRoot: '/uploads'
@@ -36,7 +45,7 @@ import {EmailModule} from "./modules/email/email.module";
     EmailModule
   ],
   controllers: [AppController],
-  providers: [AppService,],
+  providers: [AppService],
 })
 // export class AppModule implements NestModule {
 // 应用中间件
