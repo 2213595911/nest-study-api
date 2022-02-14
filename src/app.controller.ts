d51f9dc6bded66a722253678500ecd5bf3e996c1
luca <2213595911@qq.com>
2022-02-14 15:20:52 +0800

    添加帖子、用户、登录模块，token鉴权

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
-import {Controller, Get} from '@nestjs/common';
+import {Controller, Get, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import {Express} from 'express'
 import {AppService} from './app.service';
-import {ApiTags} from '@nestjs/swagger';
+import {ApiOperation,  ApiTags} from '@nestjs/swagger';
+import {FileInterceptor} from "@nestjs/platform-express";
+import {ApiImplicitFile} from "@nestjs/swagger/dist/decorators/api-implicit-file.decorator";
 
 @Controller('/')
 @ApiTags('默认')
@@ -8,8 +11,21 @@ export class AppController {
   constructor(private readonly appService: AppService) {
   }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @ApiOperation({summary: '默认接口'})
+  index() {
+    return 'hellow world'
+  }
+
+  @Post('upload')
+  @ApiImplicitFile({
+    name: '头像',
+    description: '上传头像',
+    required: true,
+  })
+  @ApiOperation({summary: "上传文件"})
+  @UseInterceptors(FileInterceptor('file'))
+  uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.upload(file)
   }
 }
